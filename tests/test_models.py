80a1fffe07b130a4de5a317bb2b2e418f91b5442
Abhijit Das Gupta <abhijit.dasgupta@astrazeneca.com>
2022-02-15 09:53:17 -0500

    Add parametrization mean test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -75,3 +75,15 @@ def test_daily_min_string():
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello','there'], ['General','Kenobi']])
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
